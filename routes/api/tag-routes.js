71a43411b564107818f6cc541f37bbd2f32c50f5
Bill Morris <bilcon@swbell.net>
2021-12-19 06:35:09 -0600

    updated category and product routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,51 +3,38 @@ const { Tag, Product, ProductTag, Category } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag.findAll({
-    include: [
-      {
-        model: Product,
-        attributes: ["id", "product_name", "price", "stock", "category_id"],
-      },
-    ],
-  })
-    .then((dbTagData) => res.json(dbTagData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const newTag = await Tag.findAll({
+      include: [{ model: Product }],
     });
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  Tag.findOne({
-    where: {
-      id: req.params.id,
-    },
-    include: [
-      {
-        model: Product,
-        attributes: ["id", "product_name", "price", "stock", "category_id"],
-      },
-    ],
-  })
-    .then((dbTagData) => {
-      if (!dbTagData) {
-        res
-          .status(404)
-          .json({ message: "There was no tag located with this id" });
-        return;
-      }
-      res.json(dbTagData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const newTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
     });
+
+    if (!readerData) {
+      res
+        .status(404)
+        .json({ message: "There was no tag located with this id" });
+      return;
+    }
+
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
